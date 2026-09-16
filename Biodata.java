46fc6c38acf067389cc6a8a2e36eece3a727e410
M. Baharudin Wakhid <varie0167@gmail.com>
2026-09-16 15:53:18 +0700

    Add Biodata class to display personal information

---
+++ Biodata.java
@@ -0,0 +1,8 @@
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("Nama     : M. BAHARUDIN WAKHID");
+        System.out.println("NIM      : 261301019");
+        System.out.println("Jurusan  : Sistem Komputer");
+        System.out.println("Universitas: Universitas Nahdlatul Ulama");
+    }
+}
